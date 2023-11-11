28bdd1d89ad37d6b5d0b4aba80a154dd0bac9298
ashu-dev-ops <computerlogic25@gmail.com>
2023-11-11 09:54:44 +0530

    images fixed

--- next.config.js
+++ next.config.js
@@ -1,6 +1,38 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // images: {
+  //   remotePatterns: [
+  //     {
+  //       protocol: "https",
+  //       hostname: "**",
+  //     },
+  //   ],
+  // },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+
+  // basePath: "https://nimble-figolla-41e82f.netlify.app/",
+  // basePath: "http://localhost:3000/",
+  // assetPrefix: "https://nimble-figolla-41e82f.netlify.app/",
+  assetPrefix: "http://localhost:3000/",
+  // images: {
+  //   domains: [process.env.VERCEL_URL],
+  //   path: `${process.env.VERCEL_URL}/_next/image`,
+  // },
+  // images: {
+  //   domains: ["http://localhost:3000/"],
+  //   path: `${"http://localhost:3000"}/_next/image`,
+  //   remotePatterns: [
+  //     {
+  //       protocol: "https",
+  //       hostname: "**",
+  //     },
+  //   ],
+  // },
   images: {
+    domains: ["https://nimble-figolla-41e82f.netlify.app/"],
+    path: `${"https://nimble-figolla-41e82f.netlify.app"}/_next/image`,
     remotePatterns: [
       {
         protocol: "https",
@@ -8,12 +40,6 @@ const nextConfig = {
       },
     ],
   },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-
-  // basePath: "https://nimble-figolla-41e82f.netlify.app/",
-  assetPrefix: "https://nimble-figolla-41e82f.netlify.app/",
   // async rewrites() {
   //   return [
   //     // Rewrite assets to use the correct absolute path
